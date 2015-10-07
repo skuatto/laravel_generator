7d02f009c21e5ad6ffbe7573b39be7414a366010
skuatto <t.clody@gmail.com>
2015-10-07 17:21:28 +0200

    Adatar la base de dades per test, en un futur en canviara

--- test_script.sql
+++ test_script.sql
@@ -1,1 +1,11 @@
-﻿
+﻿CREATE TABLE `test`.`contacto` (
+  `idcontacto` INT NOT NULL,
+  `descripcion` VARCHAR(255) NULL,
+  `pdf` VARCHAR(255) NULL,
+  PRIMARY KEY (`idcontacto`));
+
+CREATE TABLE `test`.`informacio` (
+  `idinformacio` INT NOT NULL,
+  `descripcion` VARCHAR(255) NULL,
+  `data` DATETIME NULL,
+  PRIMARY KEY (`idinformacio`));
